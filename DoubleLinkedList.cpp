6f8de5076a1179347a908791f9f089ffddb8fc86
salmaarayyan <salmaarayyan@gmail.com>
2024-05-04 22:33:47 +0700

    memeriksa apakah linked list sudah ada node dengan nomor mahasiswa yang sama dengan node baru

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -21,7 +21,14 @@ void addNode()
 
     //insert the new node in the list
     if (START == NULL || newNode->noMhs == START->noMhs)
-    {
-        //step 2 : insert the new node at the beginning
+    {  //step 2 : insert the new node at the beginning
+        
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+
+        
     }
 }
