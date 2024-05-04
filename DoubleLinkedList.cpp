a0c07e97385fc7f8422319d4286ced73036fdd5a
salmaarayyan <salmaarayyan@gmail.com>
2024-05-04 22:27:52 +0700

    display dan accept noMhs dan name

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -14,4 +14,8 @@ Node *START = NULL;
 void addNode()
 {
     Node *newNode = new Node();  //step 1 : create a new node
+    cout << "\nEnter the roll number of the student : ";
+    cin >> newNode->noMhs;  //assign value to the data field of the new node
+    cout << "\nEnter the name of the student : ";
+    cin >> newNode->name;  //assign value to the data field of the new node
 }
