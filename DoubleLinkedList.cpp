0c79ef55582f09e84249c16997cdbc74633eacb5
salmaarayyan <salmaarayyan@gmail.com>
2024-05-04 22:42:04 +0700

    membuat pointer

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -39,6 +39,8 @@ void addNode()
     }
     else
     {
-        
+        //insert the new node in the middle or at the end
+        Node *current = START; //step 1.a : start from the first node
+        Node *previous = NULL; //step 1.b : previous node is NULL initially
     }
 }
